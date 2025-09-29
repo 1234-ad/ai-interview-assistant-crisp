fcced16d2399c6e7399e4ed4ff7a4cc0d5896c64
Aditya <97450298+1234-ad@users.noreply.github.com>
2025-09-29 11:00:30 +0530

    Add src/index.js with Redux and Ant Design setup

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { ConfigProvider } from 'antd';
+import { store, persistor } from './store/store';
+import App from './App';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <ConfigProvider
+          theme={{
+            token: {
+              colorPrimary: '#1890ff',
+            },
+          }}
+        >
+          <App />
+        </ConfigProvider>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
